f4637b5488ab4488825c3fd6adecbf2b0ab0d0b3
Jaime Still <jpstill85@gmail.com>
2022-06-17 20:48:46 -0400

    reactive prototypes complete

---
+++ src/App.Web/Hubs/HubExtensions.cs
@@ -0,0 +1,8 @@
+namespace App.Web.Hubs;
+public static class HubExtensions
+{
+    public static void MapHubs(this IEndpointRouteBuilder endpoint)
+    {
+        // endpoint.MapHub<Hub>("/endpoint");
+    }
+}
